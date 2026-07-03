96a8c2a6594be6bb2cd17a123233d43e5ad4c4be
Joaquim Cardeira <joaquim.cardeira@gmail.com>
2026-07-03 16:16:46 +0100

    moviflotte.com

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,7 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
-    url.host = env.TRACCAR_SERVER || 'gps.fleetmap.pt'
-    url.protocol = 'http:'
-    url.port = 80
+    url.host = env.TRACCAR_SERVER || 'moviflotte.com'
     return fetch(new Request(url, request), cf)
 }
