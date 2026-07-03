c0812ee90a2e77aa00dbbd1a5e487acb7bb409a5
Joaquim Cardeira <joaquim.cardeira@gmail.com>
2026-07-03 16:25:12 +0100

    12

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,5 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
     url.host = env.TRACCAR_SERVER || 'moviflotte.com'
-    return fetch(new Request(url, request), cf)
+    return fetch(new Request(url, request, { redirect: 'follow' }), cf)
 }
